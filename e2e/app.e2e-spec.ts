9583d1b99dff1162ded1183923b659ec20c5b943
mspalti <mspalti@gmail.com>
2017-03-09 16:14:26 -0800

    chore: Updated simple e2e tests to work with new project structure.
    Also, while at it I found and corrected the warning generated by angular material components re: unable to find hammer.js.
    
    Issues: AC-916

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('commons App', function() {
 
   it('should display message saying app works', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('Academic Commons Prototype');
   });
 });
--- src/client/polyfills.ts
+++ src/client/polyfills.ts
@@ -45,6 +45,9 @@ import 'core-js/es6/reflect';
 import 'core-js/es7/reflect';
 
 
+/** Hammerjs required by angular material. **/
+import 'hammerjs/hammer';
+
 /** ALL Firefox browsers require the following to support `@angular/animation`. **/
 // import 'web-animations-js';  // Run `npm install --save web-animations-js`.
 
