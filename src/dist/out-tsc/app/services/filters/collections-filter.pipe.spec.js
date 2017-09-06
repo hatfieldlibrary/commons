a5bf1e7bb3aed7765063511234540e8609f024a6
Michael W Spalti <mspalti@wu-164283.willamette.edu>
2017-09-06 09:41:57 -0700

    chore: Updated .gitignore to ignore typescript out directory.
    Issues: AC-1009

--- src/dist/out-tsc/app/services/filters/collections-filter.pipe.spec.js
+++
@@ -1,8 +0,0 @@
-import { CollectionsFilterPipe } from './collections-filter.pipe';
-describe('CollectionsFilterPipe', function () {
-    it('create an instance', function () {
-        var pipe = new CollectionsFilterPipe();
-        expect(pipe).toBeTruthy();
-    });
-});
-//# sourceMappingURL=/Users/mspalti/willamette/commons/commons-6.28.17/src/client/app/services/filters/collections-filter.pipe.spec.js.map
