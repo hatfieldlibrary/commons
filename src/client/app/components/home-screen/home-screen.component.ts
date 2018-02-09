897e806c2fbff305b18392527a5e9cc71955d697
Michael W Spalti <mspalti@gmail.com>
2018-02-09 15:33:36 -0800

    feat: Moe color adjustments in the list view.
    Also, added tweak to collection reducers to set collections to empty list for new actions.
    
    Issues: AC-1209 AC-1216

--- src/client/app/components/home-screen/home-screen.component.ts
+++ src/client/app/components/home-screen/home-screen.component.ts
@@ -1,9 +1,10 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-home-screen',
   templateUrl: './home-screen.component.html',
   styleUrls: ['./home-screen.component.css'],
+  encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomeScreenComponent  {
