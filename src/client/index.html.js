8352be8b10bd7aea7974f752a5738be3a913666e
Michael W Spalti <mspalti@gmail.com>
2018-03-22 12:40:59 -0700

    chore: minor documentation update

---
+++ src/client/index.html.js
@@ -0,0 +1,318 @@
+html >
+    --;
+~Copyright(c);
+2017.;
+~~This;
+program;
+is;
+free;
+software: you;
+can;
+redistribute;
+it;
+and / or;
+modify;
+~it;
+under;
+the;
+terms;
+of;
+the;
+GNU;
+General;
+Public;
+License;
+by;
+~the;
+Free;
+Software;
+Foundation, either;
+version;
+3;
+of;
+the;
+License, or;
+~(at);
+your;
+option;
+any;
+later;
+version.
+;
+~~This;
+program;
+is;
+distributed in the;
+hope;
+that;
+it;
+will;
+be;
+useful,
+    ~but;
+WITHOUT;
+ANY;
+WARRANTY;
+without;
+even;
+the;
+implied;
+warranty;
+of;
+~MERCHANTABILITY;
+or;
+FITNESS;
+FOR;
+A;
+PARTICULAR;
+PURPOSE.See;
+the;
+~GNU;
+General;
+Public;
+License;
+for (more; details.
+; ~~You)
+    should;
+have;
+received;
+a;
+copy;
+of;
+the;
+GNU;
+General;
+Public;
+License;
+~along;
+with (this)
+    program.If;
+not, see < http;
+-- >
+    charset;
+"utf-8" >
+    Academic;
+Commons < /title>
+    < base;
+href = "/" /  >
+    name;
+"viewport";
+content = "width=device-width, initial-scale=1" >
+    rel;
+"icon";
+type = "image/x-icon";
+href = "/commons-resource/favicon.ico" >
+        .myshowbox;
+{
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    padding: 5 % ;
+}
+myloader;
+{
+    position: relative;
+    margin: 0;
+    auto;
+    width: 100;
+    px;
+}
+myloader: before;
+{
+    content: '';
+    display: block;
+    padding - top;
+    100 % ;
+}
+mycircular;
+{
+    -webkit - animation;
+    rotate;
+    2;
+    s;
+    linear;
+    infinite;
+    animation: rotate;
+    2;
+    s;
+    linear;
+    infinite;
+    height: 100 % ;
+    -webkit - transform - origin;
+    center;
+    center;
+    transform - origin;
+    center;
+    center;
+    width: 100 % ;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    margin: auto;
+}
+mypath;
+{
+    stroke - dasharray;
+    1, 400;
+    stroke - dashoffset;
+    0;
+    -webkit - animation;
+    dash;
+    1.5;
+    s;
+    ease -  in -out;
+    infinite, color;
+    6;
+    s;
+    ease -  in -out;
+    infinite;
+    animation: dash;
+    1.5;
+    s;
+    ease -  in -out;
+    infinite, color;
+    6;
+    s;
+    ease -  in -out;
+    infinite;
+    stroke - linecap;
+    round;
+}
+-webkit - keyframes;
+rotate;
+{
+    100 % {}
+        - webkit - transform;
+    rotate(360, deg);
+    transform: rotate(360, deg);
+}
+rotate;
+{
+    100 % {}
+        - webkit - transform;
+    rotate(360, deg);
+    transform: rotate(360, deg);
+}
+-webkit - keyframes;
+dash;
+{
+    0 % {
+        stroke: -dasharray, 1: , 400: ,
+        stroke: -dashoffset, 0: 
+    };
+    50 % {
+        stroke: -dasharray, 89: , 400: ,
+        stroke: -dashoffset
+    } - 35;
+    px;
+}
+100 % {
+    stroke: -dasharray, 89: , 400: ,
+    stroke: -dashoffset
+} - 124;
+px;
+dash;
+{
+    0 % {
+        stroke: -dasharray, 1: , 400: ,
+        stroke: -dashoffset, 0: 
+    };
+    50 % {
+        stroke: -dasharray, 89: , 400: ,
+        stroke: -dashoffset
+    } - 35;
+    px;
+}
+100 % {
+    stroke: -dasharray, 89: , 400: ,
+    stroke: -dashoffset
+} - 124;
+px;
+-webkit - keyframes;
+color;
+{
+    100 % ,
+        0 % {
+            stroke: , d62d20: 
+        };
+    40 % {
+        stroke: , 0057: e7
+    };
+    66 % {
+        stroke: , 00: 8744
+    };
+    80 % ,
+        90 % {
+            stroke: , ffa700: 
+        };
+}
+color;
+{
+    100 % ,
+        0 % {
+            stroke: , d62d20: 
+        };
+    40 % {
+        stroke: , 0057: e7
+    };
+    66 % {
+        stroke: , 00: 8744
+    };
+    80 % ,
+        90 % {
+            stroke: , ffa700: 
+        };
+}
+/style>
+    < /head>
+    < body;
+var default_1 = /** @class */ (function () {
+    function default_1() {
+    }
+    return default_1;
+}());
+"body" >
+    -root >
+    /** @class */ (function () {
+        function class_1() {
+        }
+        return class_1;
+    }());
+"myshowbox" >
+    /** @class */ (function () {
+        function class_2() {
+        }
+        return class_2;
+    }());
+"myloader" >
+    /** @class */ (function () {
+        function class_3() {
+        }
+        return class_3;
+    }());
+"mycircular";
+viewBox = "25 25 50 50" >
+    /** @class */ (function () {
+        function class_4() {
+        }
+        return class_4;
+    }());
+"mypath";
+cx = "50";
+cy = "50";
+r = "20";
+fill = "none";
+stroke - width;
+"4";
+stroke - miterlimit;
+"20" /  >
+    /svg>
+    < /div>
+    < /div>
+    < /app-root>
+    < /body>
+    < /html>;
